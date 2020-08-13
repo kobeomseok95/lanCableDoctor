b3adaa2058a58018c811ccd2a04aa5ce8cc790ad
오진교 <@LAPTOP-93CPOJLC>
2020-08-13 09:18:35 +0900

    2020/08/13_09:17_진교_로그인해야 구매 가능,결제 성공 후 화면 수정

--- landocProject/src/main/java/com/kh/landocProject/payment/model/vo/PayProduct.java
+++ landocProject/src/main/java/com/kh/landocProject/payment/model/vo/PayProduct.java
@@ -17,21 +17,13 @@ public class PayProduct implements Serializable{
 	private String thumbnail;
 	private int ctCode;
 	private int productCount;
+	private String pdpRename;
+	
 	protected PayProduct() {
 	}
+
 	protected PayProduct(int pdNo, String pdName, int originPrice, int discount, int sellPrice, String thumbnail,
-			int ctCode) {
-		this.pdNo = pdNo;
-		this.pdName = pdName;
-		OriginPrice = originPrice;
-		this.discount = discount;
-		this.sellPrice = sellPrice;
-		this.thumbnail = thumbnail;
-		this.ctCode = ctCode;
-	}
-	
-	protected PayProduct(int pdNo, String pdName, int originPrice, int discount, int sellPrice, String thumbnail,
-			int ctCode, int productCount) {
+			int ctCode, int productCount, String pdpRename) {
 		this.pdNo = pdNo;
 		this.pdName = pdName;
 		OriginPrice = originPrice;
@@ -40,65 +32,91 @@ public class PayProduct implements Serializable{
 		this.thumbnail = thumbnail;
 		this.ctCode = ctCode;
 		this.productCount = productCount;
+		this.pdpRename = pdpRename;
 	}
+
 	public int getPdNo() {
 		return pdNo;
 	}
+
 	public void setPdNo(int pdNo) {
 		this.pdNo = pdNo;
 	}
+
 	public String getPdName() {
 		return pdName;
 	}
+
 	public void setPdName(String pdName) {
 		this.pdName = pdName;
 	}
+
 	public int getOriginPrice() {
 		return OriginPrice;
 	}
+
 	public void setOriginPrice(int originPrice) {
 		OriginPrice = originPrice;
 	}
+
 	public int getDiscount() {
 		return discount;
 	}
+
 	public void setDiscount(int discount) {
 		this.discount = discount;
 	}
+
 	public int getSellPrice() {
 		return sellPrice;
 	}
+
 	public void setSellPrice(int sellPrice) {
 		this.sellPrice = sellPrice;
 	}
+
 	public String getThumbnail() {
 		return thumbnail;
 	}
+
 	public void setThumbnail(String thumbnail) {
 		this.thumbnail = thumbnail;
 	}
+
 	public int getCtCode() {
 		return ctCode;
 	}
+
 	public void setCtCode(int ctCode) {
 		this.ctCode = ctCode;
 	}
-	
+
 	public int getProductCount() {
 		return productCount;
 	}
+
 	public void setProductCount(int productCount) {
 		this.productCount = productCount;
 	}
+
+	public String getPdpRename() {
+		return pdpRename;
+	}
+
+	public void setPdpRename(String pdpRename) {
+		this.pdpRename = pdpRename;
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+
 	@Override
 	public String toString() {
 		return "PayProduct [pdNo=" + pdNo + ", pdName=" + pdName + ", OriginPrice=" + OriginPrice + ", discount="
 				+ discount + ", sellPrice=" + sellPrice + ", thumbnail=" + thumbnail + ", ctCode=" + ctCode
-				+ ", productCount=" + productCount + "]";
+				+ ", productCount=" + productCount + ", pdpRename=" + pdpRename + "]";
 	}
-	
+
 	
 }
