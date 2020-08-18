96788d8d1d6595ef99de21f6866f7665fbac4c2a
kobeomseok <kobeomseok@DESKTOP-O5DQE4O>
2020-08-18 16:30:50 +0900

    200818(Tue)_16:30_범석 > 메이븐에 소켓 지움

--- landocProject/src/main/java/com/kh/landocProject/handler/WebSocketHandler.java
+++ landocProject/src/main/java/com/kh/landocProject/handler/WebSocketHandler.java
@@ -1,24 +1,24 @@
-package com.kh.landocProject.handler;
-
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-public class WebSocketHandler extends TextWebSocketHandler{
-	
-	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		
-	}
-	
-	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		session.sendMessage(new TextMessage("echo : " + message.getPayload()));
-	}
-	
-	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		super.afterConnectionClosed(session, status);
-	}
-}
+//package com.kh.landocProject.handler;
+//
+//import org.springframework.web.socket.CloseStatus;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//public class WebSocketHandler extends TextWebSocketHandler{
+//	
+//	@Override
+//	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//		
+//	}
+//	
+//	@Override
+//	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+//		session.sendMessage(new TextMessage("echo : " + message.getPayload()));
+//	}
+//	
+//	@Override
+//	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+//		super.afterConnectionClosed(session, status);
+//	}
+//}
